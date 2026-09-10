dc8e855125ebcbbbc256c74df8caf26eae00220f
likhitha-dev581 <Likhitha.Sheri@cashapona.com>
2026-09-10 10:57:28 +0530

    Implemented login api and jwt

---
+++ app/core/security.py
@@ -0,0 +1,73 @@
+from datetime import datetime, timedelta, timezone
+
+from jose import jwt
+from passlib.context import CryptContext
+from app.core.config import Settings
+
+
+# Password hashing
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+
+# JWT configuration
+SECRET_KEY = Settings.jwt_secret_key
+ALGORITHM = Settings.jwt_algorithm 
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 120 
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(
+        plain_password,
+        hashed_password
+    )
+
+
+def create_access_token(user_id: int, role: str) -> str:
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    payload = {
+        "sub": str(user_id),
+        "role": role,
+        "type": "access",
+        "exp": expire
+    }
+
+    return jwt.encode(
+        payload,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+
+def create_refresh_token(user_id: int, role: str) -> str:
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        days=REFRESH_TOKEN_EXPIRE_DAYS
+    )
+
+    payload = {
+        "sub": str(user_id),
+        "role": role,
+        "type": "refresh",
+        "exp": expire
+    }
+
+    return jwt.encode(
+        payload,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+def decode_token(token: str) -> dict:
+    return jwt.decode(
+        token,SECRET_KEY,
+        algorithms=[ALGORITHM]
+    )
